e46e682a6017b86fced73d7e574ab0b3ea345bd8
Lindsey Armour <lindsarmour21@gmail.com>
2019-04-08 13:24:24 -0400

    Added method

--- module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCDepartmentDAO.java
+++ module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCDepartmentDAO.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.techelevator.projects.model.Department;
 import com.techelevator.projects.model.DepartmentDAO;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 public class JDBCDepartmentDAO implements DepartmentDAO {
 	
@@ -29,13 +30,21 @@ public class JDBCDepartmentDAO implements DepartmentDAO {
 
 	@Override
 	public List<Department> searchDepartmentsByName(String nameSearch) {
+		//create an instance of Arraylist
+		//create sql query with clause WHERE  name = ?
+		//SqlRowSet results = jdbcTemplate.queryForRowSet(sqlqueryname)
+		//return arraylist
 		return new ArrayList<>();
 	}
 
 	@Override
 	public void saveDepartment(Department updatedDepartment) {
-		
+		//sqlquery that has INSERT INTO and VALUES
+		//where(result.next())
+		//updatedDepartment.setID
+		//jdbcTemplate.update(sqlInsertCity,
 	}
+
 
 	@Override
 	public Department createDepartment(Department newDepartment) {
@@ -44,7 +53,23 @@ public class JDBCDepartmentDAO implements DepartmentDAO {
 
 	@Override
 	public Department getDepartmentById(Long id) {
+		//Department theDepartment = null;
+		//sql query with clause WHERE id = ?
+		//SqlRowSet results = jdbcTemplate.queryForRowSet(sqlqueryname, id)
+
+		//if(results.next()){  theDepartment = mapRowToDepartment
+
 		return null;
+	}
+
+	private Department mapRowToDepartment(SqlRowSet results) {
+		Department theDepartment = new Department();
+
+		theDepartment.setId(results.getLong("id"));
+		theDepartment.setName(results.getString("name"));
+
+		return theDepartment;
+
 	}
 
 }
